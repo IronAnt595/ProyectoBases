ea1f0110f2e32b198752892b67c89fef1f22bc2b
Samuel Vargas <sjvc595@gmail.com>
2023-05-30 10:10:38 -0500

    Creación de página login

--- Evaluacion/views.py
+++ Evaluacion/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the Evaluacion index.")
+
+def login(request):
+    return HttpResponse("Hello, world. You're at the Evaluacion login.")
