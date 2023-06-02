e50790e681f7326b265232ea501079c0617eefd7
Samuel Vargas <sjvc595@gmail.com>
2023-06-01 22:07:09 -0500

    authenticate en función de grupos

--- Evaluacion/backend.py
+++ Evaluacion/backend.py
@@ -3,13 +3,31 @@ from django.contrib.auth.backends import ModelBackend
 
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, group=None, **kwargs):
+        # UserModel = get_user_model()
+        # try:
+        #     user = UserModel.objects.get(username=username)
+        # except UserModel.DoesNotExist:
+        #     return None
+        # else:
+        #     if user.check_password(password):
+        #         return user
+        # return None
         UserModel = get_user_model()
         try:
             user = UserModel.objects.get(username=username)
         except UserModel.DoesNotExist:
             return None
         else:
+            print("Se encontró el usuario, verificando contraseña")
             if user.check_password(password):
-                return user
+                print("Contraseña correcta, verificando grupo")
+                print(group)
+                if group == 1 and user.groups.filter(name='Estudiante').exists():
+                    return user
+                elif group == 2 and user.groups.filter(name='Profesor').exists():
+                    return user
+                elif group == 3 and user.groups.filter(name='Administrativo').exists():
+                    return user
+        print("No se encontro el usuario")
         return None
